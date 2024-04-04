c7dbb4eaace29113fce31811b12ea3b5226766b5
Adrian Lis <01187024@pw.edu.pl>
2024-04-04 10:45:48 +0200

    Merge branch 'main' of https://gitlab-stud.elka.pw.edu.pl/lborowsk/apro2_24l_pro_pt_azul.git into main

--- src/gamestate/Loader.java
+++ src/gamestate/Loader.java
@@ -1,4 +1,15 @@
 package gamestate;
 
+import java.io.IOException;
+
 public class Loader {
+	
+	/**
+	 * Loads the game state from a file
+	 * @param filePath
+	 * @throws IOException
+	 */
+	public static void loadGame(String filePath) throws IOException {
+    }
+
 }

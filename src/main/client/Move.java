959a92cf1b4f274bc1587caf626586c104e3fd54
Jakub Zamyslewski <jakub.zamyslewski.stud@pw.edu.pl>
2024-05-10 11:06:07 +0200

    powrót do move'a pawła

--- src/main/client/Move.java
+++ src/main/client/Move.java
@@ -7,38 +7,38 @@ import java.io.Serializable;
 
 public class Move implements Serializable {
 
-  private String workshopOrMiddle;
+    private String workshopOrMiddle;
 
-  public String getWorkshopOrMiddle() {
-    return workshopOrMiddle;
-  }
+    public String getWorkshopOrMiddle() {
+        return workshopOrMiddle;
+    }
 
-  public int getRow() {
-    return row;
-  }
+    public int getRow() {
+        return row;
+    }
 
-  public Tile getColor() {
-    return color;
-  }
+    public Tile getColor() {
+        return color;
+    }
 
-  public int getPerformer() {
-    return performerID;
-  }
+    public int getPerformer() {
+        return performerID;
+    }
 
-  public int getChosenWorkshop() {
-    return this.chosenWorkshop;
-  }
+    public int getChosenWorkshop() {
+        return this.chosenWorkshop;
+    }
 
-  private int chosenWorkshop;
-  private int row;
-  private Tile color;
-  private int performerID;
+    private int chosenWorkshop;
+    private int row;
+    private Tile color;
+    private int performerID;
 
-  public Move(String workshopOrMiddle, int chosenWorkshop, Tile color, int row, int performer) {
-    this.workshopOrMiddle = workshopOrMiddle;
-    this.chosenWorkshop = chosenWorkshop;
-    this.row = row;
-    this.color = color;
-    this.performerID = performer;
-  }
+    public Move(String workshopOrMiddle, int chosenWorkshop, Tile color, int row, int performer) {
+        this.workshopOrMiddle = workshopOrMiddle;
+        this.chosenWorkshop = chosenWorkshop;
+        this.row = row;
+        this.color = color;
+        this.performerID = performer;
+    }
 }
